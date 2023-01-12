9a55e27cba4fcdbf705844ce42274c6c6f82ab02
Ayoub-ben <49066193+Ayoub-ben@users.noreply.github.com>
2023-01-12 23:03:40 +0100

    ajout pagination

--- angular projet/angularM2Intense2023-main/src/app/app.module.ts
+++ angular projet/angularM2Intense2023-main/src/app/app.module.ts
@@ -13,6 +13,8 @@ import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatPaginatorModule} from '@angular/material/paginator';
+
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -48,7 +50,8 @@ const routes: Routes = [
     path: 'assignment/:id/edit',
     component: EditAssignmentComponent,
     canActivate:[AuthGuard]
-  }
+  },
+  
 ]
 
 @NgModule({
@@ -68,7 +71,7 @@ const routes: Routes = [
     MatNativeDateModule, MatListModule, MatCardModule,
     MatCheckboxModule, MatSlideToggleModule,
     RouterModule.forRoot(routes),
-    HttpClientModule
+    HttpClientModule, MatPaginatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular projet/angularM2Intense2023-main/src/app/assignments/assignments.component.ts
+++ angular projet/angularM2Intense2023-main/src/app/assignments/assignments.component.ts
@@ -1,32 +1,73 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { AssignmentsService } from '../shared/assignments.service';
 import { Assignment } from './assignment.model';
+import { PageEvent } from '@angular/material/paginator';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+
+
 
 @Component({
   selector: 'app-assignments',
   templateUrl: './assignments.component.html',
   styleUrls: ['./assignments.component.css']
 })
+
 export class AssignmentsComponent implements OnInit {
+
+
+  currentItemsToShow= [];
   titre="Liste des devoirs";
   assignmentSelectionne!:Assignment;
   element = true;
   dateDeRendu = true;
   searchText = '';
   assignments:Assignment[] = [];
+  
+  pageEvent: PageEvent;
+  pageSlice = this.assignments.slice(0,3)
+
+
+
   constructor(private assignmentsService:AssignmentsService) {  
+
   }
-   
+  @ViewChild('paginator') paginator: MatPaginator;
+  @ViewChild('paginatorPageSize') paginatorPageSize: MatPaginator; 
+
+
+  
+
    ngOnInit(): void {
     console.log("appelé à l'initialisation du composant");
     this.assignmentsService.getAssignments()
     .subscribe(assignments  => {
       this.assignments = assignments
-     
+      this.pageSlice = this.assignments.slice(0,3)
+
     });
       
   }
 
+  OnPageChange(event : PageEvent){
+
+    const startIndex = event.pageIndex * event.pageSize
+    let endIndex = startIndex + event.pageSize;
+    this.pageSlice= this.assignments.slice(startIndex,endIndex)
+    this.getAssignment();
+  }
+
+  getAssignment() {
+    // on récupère l'id dans l'url
+    // Le + force la conversion en number
+    this.assignmentsService.getAssignments()
+    .subscribe((assignment) => {
+      if(!assignment) return;
+
+      this.assignments = assignment;
+     
+    });
+  }
   
 
   hideData() {
@@ -43,19 +84,12 @@ export class AssignmentsComponent implements OnInit {
     });
    
   }
-/*
-  showData(assignment:Assignment){
-      if(assignment.rendu == true){
-        this.element = true;
-        console.log("acc")
-      
-    }
-  }
-*/
+
   assignmentClique(assignment:Assignment){
     console.log("assignmentClique : " + assignment.nom);
     this.assignmentSelectionne = assignment;
   }
 
+  
   
 }
